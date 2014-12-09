6d26e09469a1faff94206e3717b92a285d955a13
xdubois <xd.dubois@gmail.com>
2014-12-09 09:14:06 +0100

    ARTICLE: cleaned controller

--- website/app/controllers/ArticleController.php
+++ website/app/controllers/ArticleController.php
@@ -9,6 +9,7 @@ class ArticleController extends AuthorizedController {
 	 * @return Response
 	 */
 	public function index() {
+
 		$feeds = $this->user->feeds()->get();
 		$items = [];
 		foreach ($feeds as $feed) {
@@ -21,28 +22,6 @@ class ArticleController extends AuthorizedController {
 	}
 
 	/**
-	 * Show the form for creating a new resource.
-	 * GET /article/create
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /article
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
 	 * Display the specified resource.
 	 * GET /article/{id}
 	 *
@@ -50,41 +29,6 @@ class ArticleController extends AuthorizedController {
 	 * @return Response
 	 */
 	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /article/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /article/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id) {
-		
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /article/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
 	{
 		//
 	}
