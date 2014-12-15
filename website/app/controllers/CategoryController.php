30385cfd075dae142ab7aace03f3c5f992b7b33c
xdubois <xd.dubois@gmail.com>
2014-12-15 08:51:31 +0100

    CATEGORY: update user menu when changing a feed'scategory

--- website/app/controllers/CategoryController.php
+++ website/app/controllers/CategoryController.php
@@ -3,63 +3,6 @@
 class CategoryController extends AuthorizedController {
 
 	/**
-	 * Display a listing of the resource.
-	 * GET /category
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /category/create
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /category
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 * GET /category/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /category/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
 	 * Update the specified resource in storage.
 	 * PUT /category/{id}
 	 *
@@ -72,19 +15,8 @@ class CategoryController extends AuthorizedController {
 		$feed->category_id = Input::get('category_id') == 0 ? null : Input::get('category_id');
 		$feed->save();
 
-		return Response::make('success', 200);
+		return $this->user->renderMenu();
 	}
 
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /category/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
 
 }
--- website/public/assets/js/script.js
+++ website/public/assets/js/script.js
@@ -8,15 +8,13 @@ $(function() {
 
   $('.category-update').change(function(){
     var attributs = $(this).parent().parent();
-    console.log(attributs.data('feed-id'));
     $.ajax({
       type: "POST",
       url: attributs.data('update-category-url'),
-      data: { category_id: $(this).val(), feed_id: attributs.data('feed-id') },
-      headers: {
-        'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content'),
-      }
-    })
+      data: { category_id: $(this).val(), feed_id: attributs.data('feed-id') }
+    }).done(function(data) {
+      $("#user-navbar").html(data);
+    });
   })
 
   $("span.togglefavorite").click(function () {
@@ -35,7 +33,6 @@ $(function() {
     var posting = $.post($('#ajax-url').data('read'), { id: $(this).closest('.article').attr('id'), unread : unread });
     posting.done(function (data) {
       $("#user-navbar").html(data);
-
     });
   });
 
