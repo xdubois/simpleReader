b7a9536ab6be70bcf496c3792139dfd7f7649e9d
xdubois <xd.dubois@gmail.com>
2014-12-15 08:37:04 +0100

    ARTICLE: added arraykey check

--- website/app/views/front/partial/nav.blade.php
+++ website/app/views/front/partial/nav.blade.php
@@ -13,13 +13,22 @@
   @foreach ($subscriptions as $folder => $feeds)
   <li class="nav-header">
   <a href="{{ route('article.view', $folder) }}">FOLDER 
-     {{ $folder}} ({{ $counter[$folder] }})
+     {{ $folder}} 
+      @if(isset($counter[$folder]))
+        ({{ $counter[$folder] }})
+      @endif
    </a>
  </li>
 
  @foreach ($feeds as $feed)
 
- <li><a href="{{ route('article.view', $feed->id) }}">{{ $feed->name }} ({{ $counter['feeds'][$feed->id]}})</a></li>
+ <li><a href="{{ route('article.view', $feed->id) }}">{{ $feed->name }} 
+  @if(isset($counter['feeds'][$feed->id]))
+ ({{ $counter['feeds'][$feed->id]}})
+  @endif
+
+
+ </a></li>
  @endforeach
  @endforeach
 </ul>
